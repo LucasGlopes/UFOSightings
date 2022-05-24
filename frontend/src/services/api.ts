8197aa6e92dc58ca6cbd27efb9eed9679a7a53cd
Lucas Gomes <lucasglpes@gmail.com>
2022-05-23 23:47:04 -0300

    Update baseURL

--- frontend/src/services/api.ts
+++ frontend/src/services/api.ts
@@ -1,7 +1,9 @@
 import axios from "axios";
-
+  
 const api = axios.create({
-    baseURL: 'https://localhost:8000',
+    baseURL: (process.env.NODE_ENV === 'development')
+    ? 'http://localhost:8000/'
+    : 'https://ufo-sightings-app.herokuapp.com',
 });
 
 export default api;
